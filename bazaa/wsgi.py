c53fc29e33a163b574e5a39aa9c41c81a0538ec1
graple254 <graple254@gmail.com>
2026-03-22 17:25:54 +0300

    Modify WSGI config for project name and settings
    Updated project name and Django settings module in WSGI config.

--- bazaa/wsgi.py
+++ bazaa/wsgi.py
@@ -1,20 +1,16 @@
 """
-WSGI config for bazaa project.
+WSGI config for DBMS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-path = '/home/victorokoth/leonileo'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'leonileo.settings'
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bazaa.settings')
+
 application = get_wsgi_application()
